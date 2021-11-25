4f55a531d593d1c261a71d3c4fbfc980682a63f7
Brett Saxon <brettcsaxon@gmail.com>
2021-11-25 12:15:14 -0600

    second push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+// import Header from "../src/components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="font-ttnorms">
+      {/* <Header /> */}
+      <div
+        id="site-wrapper"
+        className="flex flex-wrap items-center justify-center w-screen h-screen site-wrapper"
+      >
+        <div className="w-2/4 p-10 text-center border-2 border-gray-400 shadow-lg rounded-3xl h-3/4">
+          {/* <div className="w-3/4 "> */}
+          <h1>
+            Hey There! If you're here you're probably looking for my portfolio!
+            and I'm probably working on a freelance project or side gig or maybe
+            I'm outside, catching a concert, or checking the latest bar that
+            opened in the neighborhood...
+          </h1>
+          <br />
+          <p>This site made with React and Tailwind </p>
+          {/* </div> */}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import Link
+
+// function Header() {
+//   return<div className"w-screen h-20 bg-blue">
+//     <Link>
+//       Home
+//     </Link>
+//     <Link>
+//       Tech
+//     </Link>
+//   </div>
+//    }
+
+// export default Header;
