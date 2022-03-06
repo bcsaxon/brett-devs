71d3e6aaff36f43aedfb8cfd95a2317f647821a3
Brett Saxon <brettcsaxon@gmail.com>
2022-03-05 23:21:39 -0600

    added react-router-dom, tech and contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-function Tech() {
+function Contact() {
   return (
     <>
       <div
@@ -6,13 +6,12 @@ function Tech() {
         className="flex flex-wrap items-center justify-center w-screen p-8 site-wrapper"
       >
         <div className="w-1/3 p-10 text-center border-2 border-gray-400 shadow-lg rounded-3xl h-3/4">
-          <h1>Tech Page</h1>
-          <br />
-          <p>Enjoying React, React Native, Tailwind, TALL Stack, HTML5, CSS3</p>
+          <h1 className="mb-5">Contact Page</h1>
+          <p>brettcsaxon@gmail.com</p>
         </div>
       </div>
     </>
   );
 }
 
-export default Tech;
+export default Contact;
---
+++ src/pages/home.js
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <>
+      <div
+        id="site-wrapper"
+        className="flex flex-wrap items-center justify-center w-screen p-8 site-wrapper"
+      >
+        <div className="w-3/4 p-10 border-2 border-gray-400 shadow-lg text-cen ter sm:w-1/3 rounded-3xl h-3/4">
+          <h1 className="mb-10">
+            Hey There! You're probably looking for my portfolio. I appreciate
+            you stopping by, but I'm probably working on a freelance project or
+            a side gig or I could be outside getting some sun, catching a
+            concert, making some music, or checking the latest bar that opened
+            in the neighborhood... you get the idea...
+          </h1>
+          <p>
+            This site made with React, Tailwind, and deployed with Netlify.{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
